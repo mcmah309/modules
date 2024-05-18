2f443f28aad98bf4e5f9634b1e5ce6f934eb8866
mcmah309 <dillonmcmahon79@gmail.com>
2024-05-18 02:03:08 +0000

    refactor: Rename to modules

--- modules_test/lib/test.dart
+++ modules_test/lib/test.dart
@@ -1,11 +1,11 @@
 import 'dart:math';
 
 import 'library_x.dart';
-import 'package:modules_lint_test/library_y.dart';
+import 'package:modules_test/library_y.dart';
 // expect_lint: prefer_named_local_libs
 import 'non_library_1.dart';
 // expect_lint: prefer_named_local_libs
-import 'package:modules_lint_test/non_library_2.dart';
+import 'package:modules_test/non_library_2.dart';
 import 'package:rust_core/prelude.dart';
 
 //import 'package:';
